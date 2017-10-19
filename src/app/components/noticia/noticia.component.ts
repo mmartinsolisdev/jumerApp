08a820bd6c6c6abca9d681aab97d9b3b4fe15016
Miguel Angel <mmartinsolis.dev@gmail.com>
2017-10-19 18:22:00 -0500

    Creación de la interfaz de noticia

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-noticia',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticia.component.css']
 })
 export class NoticiaComponent implements OnInit {
-
-  constructor() { }
+  id: string;
+  noticia: {}
+  constructor(private firebaseService: FirebaseService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    console.log(this.id);
+
+    this.firebaseService.getNoticia(this.id).subscribe(noticia => {
+        this.noticia = noticia;
+        console.log(noticia);
+
+    });
   }
 
 }
