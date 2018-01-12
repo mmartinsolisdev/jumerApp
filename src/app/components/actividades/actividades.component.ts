3f7d07a8596cfb0e2250539824bfa3c865aef4fa
Miguel Angel <mmartinsolis.dev@gmail.com>
2018-01-12 17:53:05 -0500

    Se actualizó la version de Angular Cli y el archivo htaccess

--- src/app/components/actividades/actividades.component.ts
+++ src/app/components/actividades/actividades.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../_animations/fadeInAnimation';
+import { HostBinding, HostListener } from '@angular/core/src/metadata/directives';
 
 @Component({
   selector: 'app-actividades',
   templateUrl: './actividades.component.html',
   styleUrls: ['./actividades.component.css'],
   animations: [routerTransition()],
-  host: { '[@routerTransition]': '' }
+  host:  { '[@routerTransition]': '' }
 })
 export class ActividadesComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    //Regresa el Scroll al top al momento de cambiar de ruta
+    // Regresa el Scroll al top al momento de cambiar de ruta
     window.scrollTo(0, 0)
   }
 
